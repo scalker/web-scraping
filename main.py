096268c841d0541883bf3ca6525950832325144e
sahan-chinthaka <sahanchinthaka04@gmail.com>
2023-05-20 21:57:55 +0530

    Part 1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://sahan-dev.me/"
+web = requests.get(url)
+
+soup = BeautifulSoup(web.text, "html.parser")
+result = soup.find("h1")
+
+print(result.text)
